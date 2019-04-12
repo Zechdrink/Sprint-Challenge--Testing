827cfc9193dc88f2b1b40be2a260fe1bee871c10
Zech Drinkhall <zechdrink@aol.com>
2019-04-12 10:26:02 -0700

    finished writing server endpoints

--- api/server.spec.js
+++ api/server.spec.js
@@ -33,5 +33,37 @@ describe('server testing', () => {
         expect(response.body).toHaveLength(1);
     })
 
+    test('should return an empty array', async () => {
+        let response = await request(server).get('/games');
+        expect(response.body).toEqual([])
+    })
+
+    test('should get an array of games', async () => {
+        await db("games").insert({ title: "Megaman", genre: "shooter", releaseYear: 2000 })
+        await db("games").insert({title: "Yoshi", genre: "food", releaseYear: 2000 });
+        let response  = await request(server).get('/games');
+
+        expect(response.body).toEqual([{ title: "Megaman", genre: "shooter", releaseYear: 2000, id: 1  },
+        {title: "Yoshi", genre: "food", releaseYear: 2000, id: 2 }]);
+    })
+
+    test('should return status 200', async () => {
+        let response = await request(server).get('/games');
+
+        expect(response.status).toBe(200)
+    })
+
+    test('returns status 201 and 422', async () => {
+        //no data supplied response 422
+        let response = await request(server).post('/games');
+        expect(response.status).toEqual(422);
+        //correct fields supplied response 201
+        response = await request(server).post('/games').send({ title: "snake", genre: "cool"});
+        expect(response.status).toBe(201);
+        //missing one field response 422
+        response = await request(server).post('/games').send({ title: "board" });
+        expect(response.status).toBe(422)
+    })
+
 
 })
--- helpers/game-router.js
+++ helpers/game-router.js
@@ -11,4 +11,19 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.post('/', async (req, res) => {
+    try{
+        if(!req.body.genre || !req.body.title ||typeof req.body.genre !== "string" || typeof req.body.title !== "string" ||
+        req.body.genre.length < 1 || req.body.title.length < 1){
+            res.status(422).send('fill out required fields');
+        } else {
+            const game = await Games.add(req.body);
+            res.status(201).json(game)
+        }
+    } catch(error){
+        res.status(500).send("we have a prob")
+    }
+})
+
+
 module.exports = router;
